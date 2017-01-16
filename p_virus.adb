c2f3b44a686d3042f3bb41cfe134ab3c61df0333
Grego3838 <grego38380@hotmail.fr>
2017-01-16 12:26:37 +0100

    Fin partie 1

--- p_virus.adb
+++ p_virus.adb
@@ -1,11 +1,11 @@
-with sequential_IO; 
+with sequential_IO;
 with p_esiut; use p_esiut;
 
 package body p_virus is
 
 --------------- Creation et Affichage de la grille
 
-procedure CreeVectVirus (f : in out file_type; nb : in integer; V :out TV_Virus) is
+procedure CreeVectVirus (f : in out file_type; nb : in integer; V : out TV_Virus) is
 -- {f (ouvert) contient des configurations initiales,
 -- toutes les configurations se terminent par la position du virus rouge}
 -- => {V a ete initialise par lecture dans f de la partie de numero nb}
@@ -52,10 +52,22 @@ begin
 	for i in T_Lig'range loop
 		for j in T_Col'range loop
 			Ecrire(T_Piece'image(V(i,j)));
-			Ecrire(" ");
+			Ecrire("  ");
 		end loop;
 	end loop;	
 end AfficheVectVirus;
+
+procedure AfficheCase(C : in T_Piece) is
+-- {} => {Affiche B si la case est blanche, . si la case est vide et le numéro de la couleur de la pièce sinon}
+begin
+	if C = vide then
+		Ecrire(" . ");
+	elsif C = blanc then
+		Ecrire(" B ");
+	else
+		Ecrire(T_Piece'pos(c)); ecrire(" ");
+	end if;
+end AfficheCase;
 
 procedure AfficheGrille (V : in TV_Virus) is
 -- {} => {Le contenu du vecteur V est affiche dans une grille symbolisee
@@ -64,34 +76,54 @@ procedure AfficheGrille (V : in TV_Virus) is
 --			un chiffre = numero de la couleur de la piece presente dans la case
 --			le caractere 'B' = piece blanche fixe
 -- 			rien = pas une case}
+	colonne : character := T_Col'first;
+	ligne : integer := T_Lig'first;
 begin
+
+	A_la_ligne;
+
 	--1ère ligne
-	Ecrire("\  ");
+	Ecrire_ligne("Sortie");
+
+	--2ème ligne
+	Ecrire("       \   ");
 	for i in T_Col'range loop
-		Ecrire(i); Ecrire(" ");
+		Ecrire(i); Ecrire("  ");
 	end loop;
 	A_la_ligne;
 
-	--2ème ligne
-	Ecrire(" \ ");
-	for i in 1..14 loop
+	--3ème ligne
+	Ecrire("        \ ");
+	for i in 1..21 loop
 		Ecrire("=");
 	end loop;
 	A_la_ligne;
 
 	--Reste des lignes
-	for i in T_Lig'range loop
-		Ecrire(i); Ecrire("|");
-		for k in T_Col'range loop
-			if i mod 2 = 1 and k'val mod 2 = 1 then
-				if V(i,k) = vide then Ecrire(". ");
-				
-				else Ecrire("  ");
-			end if;
-		end loop;
+	while ligne <= T_Lig'last loop
+		Ecrire("      "); Ecrire(ligne); Ecrire("| ");
+		colonne := T_Col'first;
+		
+		-- Affichage des lignes paires
+		if ligne mod 2 = 0 then 
+			while colonne < T_Col'last  loop
+				Ecrire("   "); colonne := Character'val(Character'pos(colonne) + 1); 
+				AfficheCase(V(ligne,colonne)); colonne := Character'val(Character'pos(colonne) + 1);
+			end loop;
+
+		-- Affichage des lignes impaires	
+		else 
+			while colonne <= T_Col'last  loop
+				AfficheCase(V(ligne,colonne)); colonne := Character'val(Character'pos(colonne) + 1);
+				Ecrire("   "); colonne := Character'val(Character'pos(colonne) + 1); 
+			end loop;
+		end if;
+
+		ligne := ligne + 1;
 		A_la_ligne;
 	end loop;
 
+	A_la_ligne;
 end AfficheGrille;
 --------------- Fonctions et procedures pour le jeu
 
